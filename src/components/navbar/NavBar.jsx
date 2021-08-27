dec79e35a1d558ae8dbef260d4e77fc417543588
Shunji Takano <shunjit66@gmail.com>
2021-08-28 00:14:38 +1200

    added Navbar hamburger icon

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,13 +1,28 @@
 import "./NavBar.scss"
-
+import {Person, Mail} from '@material-ui/icons';
 export default function NavBar() {
     return (
-        <div className="navbar" id="navbar">
+        <div className="navbar active" id="navbar">
             <div className="wrapper">
                 <div className="left">
                     <a href="#introduction" className="home">Shunji.</a>
+                    <div className="contactsContainer">
+                        <Person className="icon"/>
+                        <span>+64 21 239 3657</span>
+                    </div>
+                    <div className="contactsContainer">
+                        <Mail className="icon"/>
+                        <span>shunjit66@gmail.com</span>
+                    </div>
+
                 </div>
-                <div className="right"></div>
+                <div className="right">
+                    <div className="hamburger">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
             </div>
         </div>
     )
