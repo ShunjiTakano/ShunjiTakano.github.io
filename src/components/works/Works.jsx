fc86a3b5b89659d8f6d9ae6d342ff9367870e818
Shunji Takano <shunjit66@gmail.com>
2021-10-06 23:55:07 +1300

    Initial work component

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,26 @@ import "./Works.scss"
 export default function Works() {
     return (
         <div className="works" id="works">
-            
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src='assets/Coretex.png' alt=""/>
+                                </div>
+                                <h2>Coretex - Software Engineer</h2>
+                                <h3>January 2021 - Present</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic recusandae, ipsam obcaecati ex temporibus reiciendis consequatur labore corrupti eius reprehenderit ad totam vitae possimus, necessitatibus enim provident neque consequuntur a?
+                                </p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
