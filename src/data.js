6bb3d261715ec2c93ef04f657f4bf1050d64dd32
Shunji Takano <shunjit66@gmail.com>
2021-10-17 16:55:23 +1300

    Updated work description and homepage

--- src/data.js
+++ src/data.js
@@ -14,7 +14,7 @@ export const workdata = [
         title: "Fraedom - Software Engineer Intern",
         date: "November 2019 - February 2020",
         desc:
-          "Worked with the Frontend, Platform and Devops teams. Integrated Coretex360's new UI with the legacy UI using React and ASP.NET. With the Devops team, I have automated the legacy patching process and created deployment pipelines for other teams. Currently in the Platform team using a cloud kafka PaaS to improve communication between Coretex devices. Working in the deployment team to push out bi-montly releases and being one of the on-call engineers.",
+          "Created a proof of concept optical character recognition application using Python, Machine learning (Keras) and AWS services (Textract, Lambda, S3). Built a fully automated end-to-end pipeline, where users can upload an image and the image is processed and results are uploaded automatically.",
         img: "./assets/FraedomTextract.png",
       },
       {
@@ -23,7 +23,7 @@ export const workdata = [
         title: "National Tsing Hua University - Research Intern",
         date: "November 2018 - February 2019",
         desc:
-          "Worked with the Frontend, Platform and Devops teams. Integrated Coretex360's new UI with the legacy UI using React and ASP.NET. With the Devops team, I have automated the legacy patching process and created deployment pipelines for other teams. Currently in the Platform team using a cloud kafka PaaS to improve communication between Coretex devices. Working in the deployment team to push out bi-montly releases and being one of the on-call engineers.",
+          "Worked under an adjunct assistant professor to conducted research, which aimed to estimate manufacturing costs of electrical transformers using natural language processing techniques. Created a web app in Django, which allowed companies to estimate the cost of manufacturing a transformer through an AI chatbot. Invited to the IIMA2019 conference in New York to present our findings.",
         img: "./assets/NTHUGroup.jpg",
       }
   
